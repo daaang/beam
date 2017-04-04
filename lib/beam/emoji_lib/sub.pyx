aaaaa934a62cbd8f0e7e435a8f8d9e7e666c0417
Matthew Alexander LaChance <mattlach@umich.edu>
2017-04-04 09:28:54 -0400

    :muscle: Prepare to start using re.compile

--- lib/beam/emoji_lib/sub.pyx
+++ lib/beam/emoji_lib/sub.pyx
@@ -15,5 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with beam. If not, see <http://www.gnu.org/licenses/>.
+from re import compile as re_compile
+
 def sub_emoji (text):
     return text.replace(":art:", "🎨")
