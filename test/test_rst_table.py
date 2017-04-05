aaaacc51ba694de5f6064a8d063390971613f6f5
Matthew Alexander LaChance <mattlach@umich.edu>
2017-04-05 15:09:59 -0400

    :white_check_mark: Data-only table prints with 2 columns

--- test/test_rst_table.py
+++ test/test_rst_table.py
@@ -125,7 +125,6 @@ class TestTwoLeftColumnTable (GivenTwoLeftColumnTable):
     def test_str_is_empty (self):
         assert_that(str(self.table), is_(equal_to("")))
 
-    @unittest.skip
     def test_str_exists_with_data (self):
         self.table.add_data("first", "second")
         assert_that(str(self.table),
