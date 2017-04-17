aaaae09433584725d8efe5de69089bef6b20c8db
Matthew Alexander LaChance <mattlach@umich.edu>
2017-04-17 16:31:59 -0400

    :house: Fix license info

--- test/lilconf/shell_generation/test_structures.py
+++ test/lilconf/shell_generation/test_structures.py
@@ -1,20 +1,20 @@
-# lilconf: Li'l Configure
+# beam: Scripts for Me
 # Copyright 2017 Matt LaChance
 #
-# This file is part of lilconf.
+# This file is part of beam.
 #
-# lilconf is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the
-# Free Software Foundation, either version 3 of the License, or (at your
+# beam is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your
 # option) any later version.
 #
-# lilconf is distributed in the hope that it will be useful, but WITHOUT
+# beam is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
 # for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with lilconf. If not, see <http://www.gnu.org/licenses/>.
+# along with beam. If not, see <http://www.gnu.org/licenses/>.
 from hamcrest import *
 import unittest
 
