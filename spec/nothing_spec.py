aaaa6a657b9629ca6cadd2e93a45676119bfdfc7
Matthew Alexander LaChance <mattlach@umich.edu>
2017-05-18 08:51:54 -0400

    :white_check_mark: Add dependency on mamba

--- spec/nothing_spec.py
+++ spec/nothing_spec.py
@@ -15,34 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with beam. If not, see <http://www.gnu.org/licenses/>.
-PYTHON3 = python3
-SETUP = $(PYTHON3) setup.py
-INSTALL = install
-bindir = ~/bin
 
-.PHONY: build install clean test
-
-build:
-	$(SETUP) build
-
-dist: build
-	$(SETUP) sdist
-	$(SETUP) bdist_wheel
-
-install: build
-	$(SETUP) install
-	$(INSTALL) bin/* $(bindir)
-
-clean:
-	-rm -r lib/beam.egg-info build dist
-	-find . -name '*.c' | xargs rm
-	-find . -name __pycache__ | xargs rm -r
-
-test: build
-	(for libdir in build/lib*; do \
-	  echo "PYTHONPATH=\"$$libdir\""; \
-	  PYTHONPATH="$$libdir" $(PYTHON3) -m unittest || exit; done)
-	(for libdir in build/lib*; do \
-	  echo "PYTHONPATH=\"$$libdir\""; \
-	  PYTHONPATH="$$libdir" mamba || exit; done)
-	find test -name '*.bats' | xargs bats
+with description("test environment"):
+    with it("succeeds with no assertions"):
+        pass
